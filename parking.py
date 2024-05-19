16ed642d50ae14256baf79424faf8e15cb05a85d
Vikassni04 <167731587+Vikassni04@users.noreply.github.com>
2024-05-20 03:17:29 +0800

    Update parking.py

--- parking.py
+++ parking.py
@@ -1,33 +1,32 @@
-#from VikassniBranch
 import tkinter as tk
 from tkinter import messagebox
 
 root = tk.Tk()
 root.title('LOGIN')
-root.geometry('626x295')
+root.geometry(f'1195x733')
 
-# Load the image
 bg_image = tk.PhotoImage(file=r"/Users/vikassni_1304/Downloads/parking pic.png")
 
-# Keep a reference to the image to prevent garbage collection
 root.bg_image = bg_image
 
-# Create a label to display the background image
 bg_label = tk.Label(root, image=bg_image)
 bg_label.place(relwidth=1, relheight=1)
+
+button_info = {
+    "width": 20,
+    "height": 2,
+    "padx": 10,
+    "pady": 10,
+    "font": ('Times New Roman', 16)
+}
  
-# Create a button to trigger the function
-button = tk.Button(root, text="GUARD")
-button.pack(side="left", padx=110)  # Place the button on the left side with some padding
+button = tk.Button(root, text="GUARD", **button_info)
+button.pack(side="left", padx=150, pady=20)  
 
-button = tk.Button(root, text="STUDENT")
-button.pack(side="right", padx=110)  # Place the button on the right side with some padding
+button = tk.Button(root, text="STUDENT", **button_info)
+button.pack(side="right", padx=150, pady=20)  
 
-button = tk.Button(root, text="SIGN UP")
-button.pack(side="bottom", padx=10)  # Place the button on the bottom with some padding
-
-button = tk.Button(root, text="SIGN IN")
-button.pack(side="bottom", padx=10)  # Place the button on the bottom with some padding
-
+button = tk.Button(root, text="SIGN UP", **button_info)
+button.pack(side="bottom", pady=135)  
 
 root.mainloop() 
