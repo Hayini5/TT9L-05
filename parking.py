2bbbbe9a355609b95c7b36237935f614d3ecf88f
Vikassni04 <167731587+Vikassni04@users.noreply.github.com>
2024-05-20 02:39:10 +0800

    Update parking.py

--- parking.py
+++ parking.py
@@ -1,13 +1,13 @@
-#from main branch
+#from VikassniBranch
 import tkinter as tk
 from tkinter import messagebox
 
 root = tk.Tk()
-root.title('MMU')
+root.title('LOGIN')
 root.geometry('626x295')
 
 # Load the image
-bg_image = tk.PhotoImage(file=r"C:\Users\KARTHIGHAYINI\Downloads\parkinglot.png")
+bg_image = tk.PhotoImage(file=r"/Users/vikassni_1304/Downloads/parking pic.png")
 
 # Keep a reference to the image to prevent garbage collection
 root.bg_image = bg_image
@@ -26,7 +26,7 @@ button.pack(side="right", padx=110)  # Place the button on the right side with s
 button = tk.Button(root, text="SIGN UP")
 button.pack(side="bottom", padx=10)  # Place the button on the bottom with some padding
 
-button = tk.Button(root, text="SIGN in")
+button = tk.Button(root, text="SIGN IN")
 button.pack(side="bottom", padx=10)  # Place the button on the bottom with some padding
 
 
