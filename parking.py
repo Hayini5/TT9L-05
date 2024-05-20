a8e6e0ab29f3961a461c885bac8ee478095a258f
Vikassni04 <167731587+Vikassni04@users.noreply.github.com>
2024-05-20 09:55:20 +0800

    Update parking.py

--- parking.py
+++ parking.py
@@ -12,6 +12,9 @@ root.bg_image = bg_image
 bg_label = tk.Label(root, image=bg_image)
 bg_label.place(relwidth=1, relheight=1)
 
+welcome_message = tk.Label(root, text="Welcome to Parking Reservation System!", font=("Arial", 28))
+welcome_message.pack(pady=20)
+
 button_info = {
     "width": 20,
     "height": 2,
@@ -19,14 +22,19 @@ button_info = {
     "pady": 10,
     "font": ('Times New Roman', 16)
 }
+
+def button_sign_up():
+    name = tk.Label("Sign Up", "Enter your name:")
+    username = tk.Label("Sign Up", "Enter your username:")
+    password = tk.Label("Sign Up", "Enter your password:")
  
-button = tk.Button(root, text="GUARD", **button_info)
-button.pack(side="left", padx=150, pady=20)  
+button_guard = tk.Button(root, text="GUARD", **button_info)
+button_guard.pack(side="left", padx=150, pady=20)  
 
-button = tk.Button(root, text="STUDENT", **button_info)
-button.pack(side="right", padx=150, pady=20)  
+button_student = tk.Button(root, text="STUDENT", **button_info)
+button_student.pack(side="right", padx=150, pady=20)  
 
-button = tk.Button(root, text="SIGN UP", **button_info)
-button.pack(side="bottom", pady=135)  
+button_sign_up = tk.Button(root, text="SIGN UP", **button_info)
+button_sign_up.pack(side="bottom", pady=135)  
 
 root.mainloop() 
